f5cd53eeb3e7bb40a52f998b985696280710c8d9
ww57403 <ww57403@naver.com>
2021-11-23 10:32:58 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
+  return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
