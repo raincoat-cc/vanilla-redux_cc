53f272f28df7d363da2a6cde0d9d87ab4d473237
ww57403 <ww57403@naver.com>
2021-11-23 10:18:57 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-
-let count = 0;
-
-number.innerText = count;
-
-const  updateText = () => {
-  number.innerText =count;
-}
-
-const handleAdd = () => {
-  count ++;
-  updateText();
+const countModifier = (state = 0) => {
+  return state;
 };
 
-const handleMinus = () => {
-  count --;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
-
+console.log(countStore.getState());
